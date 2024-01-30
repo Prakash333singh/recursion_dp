38516feeae92b17219e536f4b8a6006baa8cb6a1
Prakash Adhikari <pa7846665@gmail.com>
2024-01-30 18:21:39 +0530

    oops 1 2 3 done

---
+++ tress/garph/map.cpp
@@ -0,0 +1,74 @@
+#include <bits\stdc++.h>
+using namespace std;
+int search(string pat, string txt)
+{
+    // code here
+    // we will store the count of each letter in map
+    unordered_map<char, int> mp;
+    for (int i = 0; i < pat.length(); i++)
+    {
+        mp[pat[i]]++;
+    }
+
+    int i = 0, j = 0;
+    int count = mp.size();
+    int ans = 0;
+    while (j < txt.length())
+    {
+
+        // calculation part
+        if (mp.find(txt[j]) != mp.end())
+        {
+            mp[txt[j]]--;
+            if (mp[txt[j]] == 0)
+                count--;
+        }
+
+        // if our window size is not equal to pat.length then
+        // we have to increment j pointer
+        if (j - i + 1 < pat.length())
+            j++;
+
+        else if (j - i + 1 == pat.length())
+        {
+            // find the ans
+            if (count == 0)
+                ans++;
+            if (mp.find(txt[i]) != mp.end())
+            {
+                mp[txt[i]]++;
+                // cout << txt[i] << endl;
+
+                if (mp[txt[i]] == 1)
+                {
+                    count++;
+                }
+            }
+            i++;
+            j++;
+        }
+    }
+    return ans;
+}
+int main()
+{
+
+    string ptr = "aaba", txt = "aabaabaa";
+    auto ans = search(ptr, txt);
+    cout << ans << "\n";
+
+    // map<char, int> mp;
+    // // set<char> s;
+    // string ptr = "aaba";
+    // for (int i = 0; i < ptr.length(); i++)
+    // {
+    //     mp[ptr[i]]++;
+    // }
+    // for (int i = 0; i < ptr.length(); i++)
+    // {
+    //     s.insert(ptr[i]);
+    // }
+    // cout << mp.size();
+    // cout << s.size();
+    return 0;
+}
